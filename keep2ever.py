dad65454495f88b652e90f4b02ce774dd091a12d
Philipp Schmitt <philipp@schmitt.co>
2022-09-04 10:39:38 +0200

    Fix paths and format with black

--- keep2ever.py
+++ keep2ever.py
@@ -1,12 +1,13 @@
 """ keep2ever """
-import io
 import argparse
-import re
-import json
 import base64
 import html
+import io
+import json
+import re
 from datetime import datetime
 from zipfile import ZipFile
+
 import magic
 from PIL import Image
 
@@ -33,50 +34,47 @@ NOTE_TEMPLATE = """<note>
 
 
 def parse_args():
-    """ Parses CLI arguments """
-    parser = argparse.ArgumentParser(
-        description='Export Google Keep to Evernote')
+    """Parses CLI arguments"""
+    parser = argparse.ArgumentParser(description="Export Google Keep to Evernote")
 
-    parser.add_argument('ImportFile', type=str,
-                        help='Google takeout export file(.zip)')
-    parser.add_argument("ExportFile", help='Output file',
-                        nargs='?', default="")
+    parser.add_argument("ImportFile", type=str, help="Google takeout export file(.zip)")
+    parser.add_argument("ExportFile", help="Output file", nargs="?", default="")
     return parser.parse_args()
 
 
 def evernote_datetime(timestamp):
-    """ Convert unix timestamp to Evernote datetime format """
+    """Convert unix timestamp to Evernote datetime format"""
     dtfts = datetime.fromtimestamp(timestamp / 1000000)
     return dtfts.strftime("%Y%m%dT%H%M%SZ")
 
 
 def evernote_resources(json_note, zip_file):
-    """ Generates "<resource>" note section from file"""
+    """Generates "<resource>" note section from file"""
     mime = magic.Magic(mime=True)
     resources = ""
 
-    for res in json_note['attachments']:
-        res_path = 'Takeout/Google Keep/' + res['filePath']
+    for res in json_note["attachments"]:
+        res_path = "Takeout/Keep/" + res["filePath"]
         try:
-            res_data = zip_file.read(res_path)
+            res_data = zip_file.read(res_path.replace("jpeg", "jpg"))
         except KeyError as ex:
             print(ex)
             continue
 
         mimetype = mime.from_buffer(res_data)
-        resources += "<resource><data encoding=\"base64\">"
+        resources += '<resource><data encoding="base64">'
         data_b64 = base64.b64encode(res_data)
 
         resources += data_b64.decode()
         resources += f"</data><mime>{mimetype}</mime>"
 
-        if 'image' in resources:
+        if "image" in resources:
             img = Image.open(io.BytesIO(res_data))
             resources += f"<width>{img.width}</width>"
             resources += f"<height>{img.height}</height>"
 
         resources += "<resource-attributes><file-name>"
-        resources += res['filePath']
+        resources += res["filePath"]
         resources += "</file-name></resource-attributes>"
         resources += "</resource>"
 
@@ -84,7 +82,7 @@ def evernote_resources(json_note, zip_file):
 
 
 def export_notes(impmort_file, export_to_file):
-    """ Export Google Keep notes (Keepout archive) to Evernote notepad file (.enex) """
+    """Export Google Keep notes (Keepout archive) to Evernote notepad file (.enex)"""
     export_filename = "GoogleKeep"
     if export_to_file != "":
         export_filename = export_to_file
@@ -95,37 +93,37 @@ def export_notes(impmort_file, export_to_file):
     try:
         notebook_file = open(export_filename, "w")
         notebook_file.write(NOTEBOOK_HEADER)
-        json_files_re = r'^Takeout\/Google Keep\/.*\.json'
-        with ZipFile(impmort_file, 'r') as zip_file:
+        json_files_re = r"^Takeout\/Keep\/.*\.json"
+        with ZipFile(impmort_file, "r") as zip_file:
             for file in zip_file.namelist():
                 if re.match(json_files_re, file):
                     json_note = json.loads(zip_file.read(file))
                     evernote_dt = evernote_datetime(
-                        json_note['userEditedTimestampUsec'])
-                    if 'textContent' in json_note:
-                        content = html.escape(json_note['textContent'])
-                        content = content.replace('\n', '<br/>\n')
-                    elif 'listContent' in json_note:
+                        json_note["userEditedTimestampUsec"]
+                    )
+                    if "textContent" in json_note:
+                        content = html.escape(json_note["textContent"])
+                        content = content.replace("\n", "<br/>\n")
+                    elif "listContent" in json_note:
                         content = "<ul>"
-                        for item in json_note['listContent']:
+                        for item in json_note["listContent"]:
                             content += f"<li>{html.escape(item['text'])}</li>"
                         content += "</ul>"
 
-                    evernote_resource = ''
+                    evernote_resource = ""
 
-                    if 'attachments' in json_note:
+                    if "attachments" in json_note:
                         evernote_resource = evernote_resources(json_note, zip_file)
 
                     template_values = {
-                        'note_title': json_note['title'],
-                        'note_created': evernote_dt,
-                        'note_updated': evernote_dt,
-                        'note_content': content,
-                        'note_resource': evernote_resource,
+                        "note_title": json_note["title"],
+                        "note_created": evernote_dt,
+                        "note_updated": evernote_dt,
+                        "note_content": content,
+                        "note_resource": evernote_resource,
                     }
 
-                    notebook_file.write(
-                        NOTE_TEMPLATE.format(**template_values))
+                    notebook_file.write(NOTE_TEMPLATE.format(**template_values))
 
         notebook_file.write(NOTEBOOK_FOOTER)
         notebook_file.close()
